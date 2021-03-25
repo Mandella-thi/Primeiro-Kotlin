77be055440de181e7f642334e3ab18c1363cadb8
Mandella-thi <thiagogarc@gmail.com>
2021-03-25 11:28:05 -0300

    contrutor

--- api-collections/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
@@ -3,7 +3,11 @@ package one.digitalinnovation.digitalonebank
 class Pessoa {
     var nome: String= "Jether"
     var cpf: String = "125.125.125.11" // private: modificador de visibilidade
-    private set
+    private set//não deixa modificar os dados
+
+    constructor()
+
+    fun pessoaInfo() ="$nome e $cpf"
     /**inner class Endereco{
         var rua: String ="Rua teste"
     }**/
@@ -12,9 +16,9 @@ class Pessoa {
 fun main(){
     val Jether= Pessoa()
     //Jether.cpf ="4567"
-    println(Jether)
+    //println(Jether)
     println(Jether.nome)
-    println(Jether.cpf)
+    println(Jether.pessoaInfo())
    // println(Jether.Endereco().rua)
 
 }
