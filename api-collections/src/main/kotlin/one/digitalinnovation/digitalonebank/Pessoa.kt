2a9c5a72104b71853dd0df4f7470843a520fbe79
Mandella-thi <thiagogarc@gmail.com>
2021-03-25 10:54:55 -0300

    private e private set

--- api-collections/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
@@ -2,12 +2,19 @@ package one.digitalinnovation.digitalonebank
 
 class Pessoa {
     var nome: String= "Jether"
-    var cpf: String = "125.125.125.11"
+    var cpf: String = "125.125.125.11" // private: modificador de visibilidade
+    private set
+    /**inner class Endereco{
+        var rua: String ="Rua teste"
+    }**/
 }
 
 fun main(){
     val Jether= Pessoa()
+    //Jether.cpf ="4567"
+    println(Jether)
     println(Jether.nome)
     println(Jether.cpf)
+   // println(Jether.Endereco().rua)
 
 }
