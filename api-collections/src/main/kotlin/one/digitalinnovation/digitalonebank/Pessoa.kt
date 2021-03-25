cecf9478d992d9932f4ab8a5936f76c192961d70
Mandella-thi <thiagogarc@gmail.com>
2021-03-25 10:05:25 -0300

    teste classes

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digitalonebank
+
+class Pessoa {
+    var nome: String= "Jether"
+    var cpf: String = "125.125.125.11"
+}
+
+fun main(){
+    val Jether= Pessoa()
+    println(Jether.nome)
+    println(Jether.cpf)
+
+}
